d4a614f3d793ce310a2b7e8e8165664e890401c3
gavin15 <gavin380@gmail.com>
2016-03-11 13:08:58 +0000

    Added new working service

---
+++ build/generated-sources/jax-ws/userws/ObjectFactory.java
@@ -0,0 +1,125 @@
+
+package userws;
+
+import javax.xml.bind.JAXBElement;
+import javax.xml.bind.annotation.XmlElementDecl;
+import javax.xml.bind.annotation.XmlRegistry;
+import javax.xml.namespace.QName;
+
+
+/**
+ * This object contains factory methods for each 
+ * Java content interface and Java element interface 
+ * generated in the userws package. 
+ * <p>An ObjectFactory allows you to programatically 
+ * construct new instances of the Java representation 
+ * for XML content. The Java representation of XML 
+ * content can consist of schema derived interfaces 
+ * and classes representing the binding of schema 
+ * type definitions, element declarations and model 
+ * groups.  Factory methods for each of these are 
+ * provided in this class.
+ * 
+ */
+@XmlRegistry
+public class ObjectFactory {
+
+    private final static QName _CreateUser_QNAME = new QName("http://UserWS/", "CreateUser");
+    private final static QName _CreateUserResponse_QNAME = new QName("http://UserWS/", "CreateUserResponse");
+    private final static QName _FileNotFoundException_QNAME = new QName("http://UserWS/", "FileNotFoundException");
+    private final static QName _Hello_QNAME = new QName("http://UserWS/", "hello");
+    private final static QName _HelloResponse_QNAME = new QName("http://UserWS/", "helloResponse");
+
+    /**
+     * Create a new ObjectFactory that can be used to create new instances of schema derived classes for package: userws
+     * 
+     */
+    public ObjectFactory() {
+    }
+
+    /**
+     * Create an instance of {@link CreateUser }
+     * 
+     */
+    public CreateUser createCreateUser() {
+        return new CreateUser();
+    }
+
+    /**
+     * Create an instance of {@link CreateUserResponse }
+     * 
+     */
+    public CreateUserResponse createCreateUserResponse() {
+        return new CreateUserResponse();
+    }
+
+    /**
+     * Create an instance of {@link FileNotFoundException }
+     * 
+     */
+    public FileNotFoundException createFileNotFoundException() {
+        return new FileNotFoundException();
+    }
+
+    /**
+     * Create an instance of {@link Hello }
+     * 
+     */
+    public Hello createHello() {
+        return new Hello();
+    }
+
+    /**
+     * Create an instance of {@link HelloResponse }
+     * 
+     */
+    public HelloResponse createHelloResponse() {
+        return new HelloResponse();
+    }
+
+    /**
+     * Create an instance of {@link JAXBElement }{@code <}{@link CreateUser }{@code >}}
+     * 
+     */
+    @XmlElementDecl(namespace = "http://UserWS/", name = "CreateUser")
+    public JAXBElement<CreateUser> createCreateUser(CreateUser value) {
+        return new JAXBElement<CreateUser>(_CreateUser_QNAME, CreateUser.class, null, value);
+    }
+
+    /**
+     * Create an instance of {@link JAXBElement }{@code <}{@link CreateUserResponse }{@code >}}
+     * 
+     */
+    @XmlElementDecl(namespace = "http://UserWS/", name = "CreateUserResponse")
+    public JAXBElement<CreateUserResponse> createCreateUserResponse(CreateUserResponse value) {
+        return new JAXBElement<CreateUserResponse>(_CreateUserResponse_QNAME, CreateUserResponse.class, null, value);
+    }
+
+    /**
+     * Create an instance of {@link JAXBElement }{@code <}{@link FileNotFoundException }{@code >}}
+     * 
+     */
+    @XmlElementDecl(namespace = "http://UserWS/", name = "FileNotFoundException")
+    public JAXBElement<FileNotFoundException> createFileNotFoundException(FileNotFoundException value) {
+        return new JAXBElement<FileNotFoundException>(_FileNotFoundException_QNAME, FileNotFoundException.class, null, value);
+    }
+
+    /**
+     * Create an instance of {@link JAXBElement }{@code <}{@link Hello }{@code >}}
+     * 
+     */
+    @XmlElementDecl(namespace = "http://UserWS/", name = "hello")
+    public JAXBElement<Hello> createHello(Hello value) {
+        return new JAXBElement<Hello>(_Hello_QNAME, Hello.class, null, value);
+    }
+
+    /**
+     * Create an instance of {@link JAXBElement }{@code <}{@link HelloResponse }{@code >}}
+     * 
+     */
+    @XmlElementDecl(namespace = "http://UserWS/", name = "helloResponse")
+    public JAXBElement<HelloResponse> createHelloResponse(HelloResponse value) {
+        return new JAXBElement<HelloResponse>(_HelloResponse_QNAME, HelloResponse.class, null, value);
+    }
+
+}
---
+++ src/java/SharesBrokeringWS/SharesBrokeringWS.java
@@ -0,0 +1,142 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package SharesBrokeringWS;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.OutputStream;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+import javax.jws.WebService;
+import javax.jws.WebMethod;
+import javax.jws.WebParam;
+import javax.xml.ws.WebServiceRef;
+import modulewebservices.CurrencyConversionWSService;
+import modulewebservices.IOException_Exception;
+import modulewebservices.MalformedURLException_Exception;
+import modulewebservices.ProtocolException_Exception;
+import org.netbeans.xml.schema.companyxmlschema.CompanyShares;
+import org.netbeans.xml.schema.companyxmlschema.CompanyType;
+import userws.FileNotFoundException_Exception;
+import userws.UserWS_Service;
+
+/**
+ *
+ * @author gavin
+ */
+@WebService(serviceName = "SharesBrokeringWS")
+public class SharesBrokeringWS {
+
+    @WebServiceRef(wsdlLocation = "WEB-INF/wsdl/localhost_13797/UserService/UserWS.wsdl")
+    private UserWS_Service service_1;
+
+    @WebServiceRef(wsdlLocation = "WEB-INF/wsdl/localhost_13797/CurrencyConvertor/CurrencyConversionWSService.wsdl")
+    private CurrencyConversionWSService service;
+
+    /**
+     * This is a sample web service operation
+     */
+    @WebMethod(operationName = "hello")
+    public String hello(@WebParam(name = "name") String txt) {
+        return "Hello " + txt + " !";
+    }
+    
+    @WebMethod(operationName = "ListCompanies")
+    public List<CompanyType> ListCompanies()
+    {
+        CompanyShares companyList= new CompanyShares();
+        List<CompanyType> companies=null;
+        try {
+            javax.xml.bind.JAXBContext jaxbCtx = javax.xml.bind.JAXBContext.newInstance(companyList.getClass().getPackage().getName());
+            javax.xml.bind.Unmarshaller unmarshaller = jaxbCtx.createUnmarshaller();
+            companyList = (CompanyShares) unmarshaller.unmarshal(new java.io.File("test.xml")); //NOI18N
+        } catch (javax.xml.bind.JAXBException ex) {
+            // XXXTODO Handle exception
+            java.util.logging.Logger.getLogger("global").log(java.util.logging.Level.SEVERE, null, ex); //NOI18N
+        }
+        return companyList.getCompany();
+    }
+
+    @WebMethod(operationName = "GetCurrencyCodes")
+    public java.util.List<java.lang.String> getCurrencyCodes() {
+        // Note that the injected javax.xml.ws.Service reference as well as port objects are not thread safe.
+        // If the calling of port operations may lead to race condition some synchronization is required.
+        modulewebservices.CurrencyConversionWS port = service.getCurrencyConversionWSPort();
+        return port.getCurrencyCodes();
+    }
+    
+    @WebMethod(operationName = "BuyShare")
+    public List<CompanyType> BuyShare(@WebParam(name = "Symbol") String company, @WebParam(name = "shareQty") String qty) throws FileNotFoundException
+    {
+        int shares = Integer.valueOf(qty);
+        CompanyShares companyList= new CompanyShares();
+        try {
+            javax.xml.bind.JAXBContext jaxbCtx = javax.xml.bind.JAXBContext.newInstance(companyList.getClass().getPackage().getName());
+            javax.xml.bind.Unmarshaller unmarshaller = jaxbCtx.createUnmarshaller();
+            companyList = (CompanyShares) unmarshaller.unmarshal(new java.io.File("test.xml")); //NOI18N
+        } catch (javax.xml.bind.JAXBException ex) {
+            // XXXTODO Handle exception
+            java.util.logging.Logger.getLogger("global").log(java.util.logging.Level.SEVERE, null, ex); //NOI18N
+        }
+        
+        DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+        Date date = new Date();
+
+        for (int i = 0; i < companyList.getCompany().size(); i++) {
+            if (companyList.getCompany().get(i).getCompanySymbol().equalsIgnoreCase(company.trim())){
+                companyList.getCompany().get(i).setAvailableShares(companyList.getCompany().get(i).getAvailableShares()-shares);
+                companyList.getCompany().get(i).getSharePrice().setLastUpdate(dateFormat.format(date));
+                break;
+            }
+        }
+        
+        try {            
+            javax.xml.bind.JAXBContext jaxbCtx = javax.xml.bind.JAXBContext.newInstance(companyList.getClass().getPackage().getName());
+            javax.xml.bind.Marshaller marshaller = jaxbCtx.createMarshaller();
+            marshaller.setProperty(javax.xml.bind.Marshaller.JAXB_ENCODING, "UTF-8"); //NOI18N
+            marshaller.setProperty(javax.xml.bind.Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
+            //marshaller.marshal(companyList, System.out);
+            OutputStream os = new FileOutputStream( "test.xml" );
+            marshaller.marshal( companyList, os );
+        } catch (javax.xml.bind.JAXBException ex) {
+            // XXXTODO Handle exception
+            java.util.logging.Logger.getLogger("global").log(java.util.logging.Level.SEVERE, null, ex); //NOI18N
+        }
+        
+        return companyList.getCompany();
+    }
+
+    /**
+     * Web service operation
+     */
+    @WebMethod(operationName = "GetTotal")
+    public String GetTotal(@WebParam(name = "shareQty") String shareQty,@WebParam(name = "sharePrice") String sharePrice, @WebParam(name = "currency1") String currency1, @WebParam(name = "currency2") String currency2) throws IOException_Exception, MalformedURLException_Exception, ProtocolException_Exception {
+        int shareQuantity = Integer.valueOf(shareQty);
+        double sharesPrice = Integer.valueOf(sharePrice);
+        double totalAmount = Double.valueOf(shareQuantity * sharesPrice);
+        
+        double conversionRate = Double.valueOf( getConversionRate(currency1,currency2));
+        double newTotalAmount = totalAmount*conversionRate;
+        return String.valueOf(newTotalAmount);
+    }
+
+    private String getConversionRate(java.lang.String arg0, java.lang.String arg1) throws IOException_Exception, MalformedURLException_Exception, ProtocolException_Exception {
+        // Note that the injected javax.xml.ws.Service reference as well as port objects are not thread safe.
+        // If the calling of port operations may lead to race condition some synchronization is required.
+        modulewebservices.CurrencyConversionWS port = service.getCurrencyConversionWSPort();
+        return port.getConversionRate(arg0, arg1);
+    }    
+    
+    @WebMethod(operationName = "CreateUser")
+    public void createUser(java.lang.String userName, java.lang.String password, java.lang.String currency) throws FileNotFoundException_Exception {
+        // Note that the injected javax.xml.ws.Service reference as well as port objects are not thread safe.
+        // If the calling of port operations may lead to race condition some synchronization is required.
+        userws.UserWS port = service_1.getUserWSPort();
+        port.createUser(userName, password, currency);
+    }
+}
